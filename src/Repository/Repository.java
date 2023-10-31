00afcff18028f87bd9d0250e637ac7e471ae94b7
Catalin <baractaru.catalin@gmail.com>
2023-10-31 17:37:45 +0200

    Continuare

--- src/Repository/Repository.java
+++ src/Repository/Repository.java
@@ -1,4 +1,12 @@
 package Repository;
+import java.util.List;
 
-public class Repository {
+public interface Repository<T> {
+    void add(T item);
+    void update(T item);
+    void delete(T item);
+    T getId(int id);
+    int getIdFromItem(T item);
+    List<T> getAll();
 }
+
