0189cceb9d9d5b09167094f1f98c03e8e3ac9956
Catalin <baractaru.catalin@gmail.com>
2023-11-14 21:02:07 +0200

    Continuare

---
+++ src/Factory/Chef.java
@@ -0,0 +1,45 @@
+package Factory;
+
+import java.util.Date;
+
+public class Chef implements Employee {
+    private int ID;
+    private String name;
+    private String address;
+    private Date hireDate;
+    private long phoneNumber;
+
+    public Chef(int ID, String name, String address, Date hireDate, long phoneNumber) {
+        this.ID = ID;
+        this.name = name;
+        this.address = address;
+        this.hireDate = hireDate;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public int getID() {
+        return ID;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public Date getHireDate() {
+        return hireDate;
+    }
+
+    @Override
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+}
+
